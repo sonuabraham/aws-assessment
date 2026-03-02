6adfd5665dd75903e70478814cbc1f8f6c27c6a7
Sonu Abraham <sonuabraham2001@gmail.com>
2026-03-02 23:37:34 +1100

    Wire up multi-region deployment in root module
    - Pass all Cognito outputs (user_pool_arn, user_pool_id, user_pool_client_id) to both regional stacks
    - Configure regional_stack_us module with us_east_1 provider
    - Configure regional_stack_eu module with eu_west_1 provider
    - Pass SNS topic ARNs to both regional stacks
    - Add ecs_cluster_name output to regional-stack module
    - Root outputs expose both regional API endpoints and Cognito details

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,12 +43,14 @@ module "regional_stack_us" {
     aws = aws.us_east_1
   }
 
-  region                = "us-east-1"
-  cognito_user_pool_arn = module.cognito.user_pool_arn
-  sns_topic_arn_lambda  = var.sns_topic_arn_lambda
-  sns_topic_arn_ecs     = var.sns_topic_arn_ecs
-  user_email            = var.user_email
-  github_repo           = var.github_repo
+  region                      = "us-east-1"
+  cognito_user_pool_arn       = module.cognito.user_pool_arn
+  cognito_user_pool_id        = module.cognito.user_pool_id
+  cognito_user_pool_client_id = module.cognito.user_pool_client_id
+  sns_topic_arn_lambda        = var.sns_topic_arn_lambda
+  sns_topic_arn_ecs           = var.sns_topic_arn_ecs
+  user_email                  = var.user_email
+  github_repo                 = var.github_repo
 }
 
 # Regional stack for eu-west-1
@@ -58,10 +60,12 @@ module "regional_stack_eu" {
     aws = aws.eu_west_1
   }
 
-  region                = "eu-west-1"
-  cognito_user_pool_arn = module.cognito.user_pool_arn
-  sns_topic_arn_lambda  = var.sns_topic_arn_lambda
-  sns_topic_arn_ecs     = var.sns_topic_arn_ecs
-  user_email            = var.user_email
-  github_repo           = var.github_repo
+  region                      = "eu-west-1"
+  cognito_user_pool_arn       = module.cognito.user_pool_arn
+  cognito_user_pool_id        = module.cognito.user_pool_id
+  cognito_user_pool_client_id = module.cognito.user_pool_client_id
+  sns_topic_arn_lambda        = var.sns_topic_arn_lambda
+  sns_topic_arn_ecs           = var.sns_topic_arn_ecs
+  user_email                  = var.user_email
+  github_repo                 = var.github_repo
 }
--- terraform/modules/regional-stack/outputs.tf
+++ terraform/modules/regional-stack/outputs.tf
@@ -17,3 +17,8 @@ output "ecs_cluster_arn" {
   description = "ARN of the ECS cluster"
   value       = aws_ecs_cluster.main.arn
 }
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
+}
